af0f0d82144135923c44150e2d76c92a263ab771
Alex Knotwell <aknotwell@outlook.com>
2025-06-01 20:10:34 -0700

    Fixed grammar on page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,9 @@ function App() {
   return (
    <div >
       <h1>User Metrics</h1>
-      <h2>Note: Filtering by name or date is case sensitive.</h2>
+      <h2><strong>Note:</strong> Filtering by name or date is case sensitive.</h2>
       <UserTable/>
-      <h2>All users highlighted in red either: haven't changed their password in over a year or haven't accessed their account in 90+ days.</h2>
+      <h2>All users highlighted in red either haven't changed their password in over a year or haven't accessed their account in 90+ days.</h2>
     </div>
 
   )
